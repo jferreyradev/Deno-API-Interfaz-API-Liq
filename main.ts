b761183ad4c1419849224c9d5637fa00dd816789
José R. Ferreyra <jferreyra.dev@gmail.com>
2024-02-15 11:12:55 -0300

    boleta adjunta en prod

--- main.ts
+++ main.ts
@@ -6,22 +6,29 @@ const URL_API = "http://200.55.244.26:3005"
 
 async function handler(req: Request): Promise<Response> {
 
-  const match1 = MID_API_BOLETA.exec(req.url);
+  const match1 = MID_API_BOLETA.exec(req.url)
 
   if (match1) {
 
     try {
       const URL = URL_API + match1.pathname.input + (match1.search.input?'?'+match1.search.input:"")
+      //console.log('fetch to: '+ URL)
       const resp = await fetch(URL,{
         headers: {
           accept: "application/pdf",
         },
       })     
+
+      const fn = resp.headers.get('content-disposition')
+
+      //console.log(fn.split("=")[1])
+
       return new Response(resp.body, {
         status: 200 ,
         headers: {
           "Access-Control-Allow-Origin":"*",
-          "content-type": "application/pdf",
+          "content-type": "application/octet-stream",
+          "Content-Disposition":"attachment; filename = " + fn.split("=")[1] + ".pdf"
         },
       });
 
@@ -30,6 +37,7 @@ async function handler(req: Request): Promise<Response> {
         status: 404,
       });
     }
+
   }
   
   const match = MID_API.exec(req.url);
@@ -38,6 +46,7 @@ async function handler(req: Request): Promise<Response> {
 
     try {
       const URL = URL_API + match.pathname.input + (match.search.input?'?'+match.search.input:"")
+      //console.log('fetch to: '+ URL)
       const resp = await fetch(URL,{
         headers: {
           accept: "application/json",
@@ -57,7 +66,6 @@ async function handler(req: Request): Promise<Response> {
       });
     }
   }
-
 
   return new Response("Not found path", {
     status: 404,
