2149a86409fd5ab7ddbef10a92377c99e7536e03
José R. Ferreyra <jferreyra.dev@gmail.com>
2024-02-15 09:43:54 -0300

    Soporte json y pdf

--- main.ts
+++ main.ts
@@ -1,16 +1,46 @@
 
-//const MID_API = new URLPattern({ pathname: "/api/:action/:en/*?" });
-const MID_API = new URLPattern({ pathname: "/api/*?" });
+const MID_API = new URLPattern({ pathname: "/api/:action/:en/*?" });
+const MID_API_BOLETA = new URLPattern({ pathname: "/api/boleta*?" });
 
-//const URL_API = "http://www.serverburru2.duckdns.org:3005";
+const URL_API = "http://www.serverburru2.duckdns.org:3005";
 
-const URL_API = Deno.env.get("URL_API");
+//const URL_API = Deno.env.get("URL_API");
 
 async function handler(req: Request): Promise<Response> {
+
+  const match1 = MID_API_BOLETA.exec(req.url)
+
+  if (match1) {
+
+    try {
+      const URL = URL_API + match1.pathname.input + (match1.search.input?'?'+match1.search.input:"")
+      //console.log('fetch to: '+ URL)
+      const resp = await fetch(URL,{
+        headers: {
+          accept: "application/pdf",
+        },
+      })     
+      return new Response(resp.body, {
+        status: 200 ,
+        headers: {
+          "Access-Control-Allow-Origin":"*",
+          "content-type": "application/pdf",
+        },
+      });
+
+    } catch (error) {
+      return new Response(error, {
+        status: 404,
+      });
+    }
+
+  }
   
   const match = MID_API.exec(req.url);
 
   if (match) {
+
+    console.log(match)
 
     try {
       const URL = URL_API + match.pathname.input + (match.search.input?'?'+match.search.input:"")
