b954603d1f4e8b77cdd079498dae51bb5539eda9
José R. Ferreyra <jferreyra.dev@gmail.com>
2024-10-01 23:37:11 -0300

    soporte sp 1

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-
 const MID_API = new URLPattern({ pathname: "/api/:action/:en/*?" });
 const MID_API_BOLETA = new URLPattern({ pathname: "/api/boleta*?" });
 
@@ -7,60 +6,91 @@ const URL_API = "http://www.serverburru2.duckdns.org:3005";
 //const URL_API = Deno.env.get("URL_API");
 
 async function handler(req: Request): Promise<Response> {
-
-  const match1 = MID_API_BOLETA.exec(req.url)
+  const match1 = MID_API_BOLETA.exec(req.url);
 
   if (match1) {
-
     try {
-      const URL = URL_API + match1.pathname.input + (match1.search.input?'?'+match1.search.input:"")
+      const URL =
+        URL_API +
+        match1.pathname.input +
+        (match1.search.input ? "?" + match1.search.input : "");
       //console.log('fetch to: '+ URL)
-      const resp = await fetch(URL,{
+      const resp = await fetch(URL, {
         headers: {
           accept: "application/pdf",
         },
-      })     
+      });
 
-      const fn = resp.headers.get('content-disposition')
+      const fn = resp.headers.get("content-disposition");
 
       //console.log(fn.split("=")[1])
 
       return new Response(resp.body, {
-        status: 200 ,
+        status: 200,
         headers: {
-          "Access-Control-Allow-Origin":"*",
+          "Access-Control-Allow-Origin": "*",
           "content-type": "application/pdf",
-          "Content-Disposition":"attachment; filename = " + fn.split("=")[1] + ".pdf"
+          "Content-Disposition":
+            "attachment; filename = " + fn.split("=")[1] + ".pdf",
         },
       });
-
     } catch (error) {
       return new Response(error, {
         status: 404,
       });
     }
-
   }
-  
+
   const match = MID_API.exec(req.url);
 
   if (match) {
-
     try {
-      const URL = URL_API + match.pathname.input + (match.search.input?'?'+match.search.input:"")
-      //console.log('fetch to: '+ URL)
-      const resp = await fetch(URL,{
-        headers: {
-          accept: "application/json",
-        },
-      })     
-      return new Response(resp.body, {
-        status: 200 ,
-        headers: {
-          "Access-Control-Allow-Origin":"*",
-          "content-type": "application/json",
-        },
-      });
+      const URL =
+        URL_API +
+        match.pathname.input +
+        (match.search.input ? "?" + match.search.input : "");
+
+      console.log("fetch to: " + URL);
+      console.log("match to: " + match.pathname.groups.action);
+      console.log("match to: " + match.pathname.groups.en);
+      console.log(req.body)
+
+      if (match.pathname.groups.action == "view" || match.pathname.groups.en == "list" ) {
+        const resp = await fetch(URL, {
+          headers: {
+            accept: "application/json",
+          },
+        });
+
+        return new Response(resp.body, {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "content-type": "application/json",
+          },
+        });
+      }
+
+      if (match.pathname.groups.action == "sp") {
+
+        const resp = await fetch(URL, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(req.body)
+        });
+
+        console.log(req)
+        
+        return new Response(resp.body, {
+          status: 200,
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "content-type": "application/json",
+          },
+        });
+      }
 
     } catch (error) {
       return new Response(error, {
@@ -72,7 +102,6 @@ async function handler(req: Request): Promise<Response> {
   return new Response("Not found path", {
     status: 404,
   });
-
 }
 
 Deno.serve(handler);
